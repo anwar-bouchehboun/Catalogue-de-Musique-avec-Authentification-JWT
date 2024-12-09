962b869ee44eab3a55da17ca1f1ebcc69808ad3e
anouar <anouar.ab95@gmail.com>
2024-12-09 20:20:24 +0100

    CDMAAJ-10 genner Envirenment

--- musique/src/main/java/com/catalogue/musique/mapper/UserMapper.java
+++ musique/src/main/java/com/catalogue/musique/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package com.catalogue.musique.mapper;
 
+import com.catalogue.musique.dto.request.UserRequest;
+import com.catalogue.musique.dto.response.UserResponse;
+import com.catalogue.musique.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+@Mapper(componentModel = "spring")
+
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserResponse toDto(User user);
+
+
+    User toEntity(UserRequest request);
 }
